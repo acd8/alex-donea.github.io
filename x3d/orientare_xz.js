141631f2ad4675aad025eb04fd8ef35736705170
Alex Donea <acd8@github.com>
2018-03-16 19:20:41 +0200

    axes v2

--- x3d/orientare_xz.js
+++ x3d/orientare_xz.js
@@ -1,10 +1,13 @@
 window.addEventListener("deviceorientation", on_device_orientation);
 
 
-
+var radius=20;
 
 function on_device_orientation(e){
   document.getElementById("id_orientation").innerHTML=e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " "+ e.gamma.toFixed(1);
   var viewpoint=document.getElementById("id_viewpoint");
-  viewpoint.setAttribute("orientation","0 0 1 " + e.alpha * Math.PI / 180);
+  var x=radius * Math.cos(e.alpha * Math.PI /180);
+  var z=radius * Math.sin(e.alpha * Math.PI /180);
+  viewpoint.setAttribute("position", x+ " 0 "+z)
+  viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
 }
