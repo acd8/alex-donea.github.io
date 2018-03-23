c0c64aa6e3f0872e439e41570f2f8f2b76c42b7e
Alex Donea <acd8@github.com>
2018-03-23 19:09:37 +0200

    x3d ball and cylinder

--- x3d/ball.js
+++ x3d/ball.js
@@ -1,17 +1,17 @@
 document.getElementById("id_business_level").innerHTML = "Business version = 2018.03.23.0";
-
-setInterval(deseneaza,30);
+var stare={y:0,directie:1};
+setInterval(deseneaza,30,stare);
 var y = 0;
 var directie = 1;
-function deseneaza()
+function deseneaza(x)
 {
 	var t = document.getElementById("id_transform");
-	t.setAttribute("translation","0 " + y + " ");
-	y += directie;
-	if (y > 10)
-		directie = -1;
+	t.setAttribute("translation","0 " + x.y + " ");
+	x.y += x.directie;
+	if (x.y > 10)
+		x.directie = -1;
 	else
-		if (y < 0)
-			directie = 1;
+		if (x.y < 0)
+			x.directie = 1
 	
 }
